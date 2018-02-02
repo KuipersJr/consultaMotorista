c78f42c93e8ae5c2a40f8d720ea256b6d608db80
KuipersJr <kuipersjr@gmail.com>
2018-02-02 21:57:19 -0200

    LoginConsulta

---
+++ MotoristaMvc5/LoginConsulta/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginConsulta.MvcApplication" Language="C#" %>
---
+++ MotoristaMvc5/LoginConsulta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginConsulta.Startup))]
+namespace LoginConsulta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
